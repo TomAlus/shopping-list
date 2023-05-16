cea966e2a8c7cd47eff9fbe7b9549830ddfedbba
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-05-16 14:24:29 +0200

    Check UI for existing items in the list

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const itemForm = document.getElementById("item-form");
-const itemInput = document.getElementById("item-input");
-const itemList = document.getElementById("item-list");
-const clearButton = document.getElementById("clear");
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const itemFilter = document.getElementById('filter');
+const clearButton = document.getElementById('clear');
 
 const createIcon = (classes) => {
-  const icon = document.createElement("i");
+  const icon = document.createElement('i');
   icon.className = classes;
   return icon;
 };
 
 const createButton = (classes) => {
-  const button = document.createElement("button");
+  const button = document.createElement('button');
   button.className = classes;
-  const icon = createIcon("fa-solid fa-xmark");
+  const icon = createIcon('fa-solid fa-xmark');
   button.appendChild(icon);
   return button;
 };
@@ -23,25 +24,27 @@ const addItem = (e) => {
   const newItem = itemInput.value;
 
   // Validate input
-  if (newItem === "") {
-    alert("Please add an item");
+  if (newItem === '') {
+    alert('Please add an item');
     return;
   }
 
   // Create list item
-  const li = document.createElement("li");
+  const li = document.createElement('li');
   li.appendChild(document.createTextNode(newItem));
 
-  const button = createButton("remove-item btn-link text-red");
+  const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
   itemList.appendChild(li);
-  itemInput.value = "";
+  checkUI();
+  itemInput.value = '';
 };
 
 // Delete item
 const removeItem = (e) => {
-  if (e.target.parentElement.classList.contains("remove-item")) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
     e.target.parentElement.parentElement.remove();
+    checkUI();
   }
 };
 
@@ -50,9 +53,23 @@ const clearItems = (e) => {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  checkUI();
+};
+
+const checkUI = () => {
+  const listItems = itemList.querySelectorAll('li');
+  if (listItems.length) {
+    clearButton.style.display = 'block';
+    itemFilter.style.display = 'block';
+  } else {
+    clearButton.style.display = 'none';
+    itemFilter.style.display = 'none';
+  }
 };
 
 // Event Listeners
-itemForm.addEventListener("submit", addItem);
-itemList.addEventListener("click", removeItem);
-clearButton.addEventListener("click", clearItems);
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItems);
+
+checkUI();
