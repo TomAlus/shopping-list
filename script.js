b1a87a3adfb3a80e1776d76c4c276666a1e70b42
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-05-17 10:20:39 +0200

    Item filtering

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const itemForm = document.getElementById('item-form');
-const itemInput = document.getElementById('item-input');
-const itemList = document.getElementById('item-list');
-const itemFilter = document.getElementById('filter');
-const clearButton = document.getElementById('clear');
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const itemFilter = document.getElementById("filter");
+const clearButton = document.getElementById("clear");
 
 const createIcon = (classes) => {
-  const icon = document.createElement('i');
+  const icon = document.createElement("i");
   icon.className = classes;
   return icon;
 };
 
 const createButton = (classes) => {
-  const button = document.createElement('button');
+  const button = document.createElement("button");
   button.className = classes;
-  const icon = createIcon('fa-solid fa-xmark');
+  button.type = "button";
+  const icon = createIcon("fa-solid fa-xmark");
   button.appendChild(icon);
   return button;
 };
@@ -24,25 +25,25 @@ const addItem = (e) => {
   const newItem = itemInput.value;
 
   // Validate input
-  if (newItem === '') {
-    alert('Please add an item');
+  if (newItem === "") {
+    alert("Please add an item");
     return;
   }
 
   // Create list item
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
 
-  const button = createButton('remove-item btn-link text-red');
+  const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
   itemList.appendChild(li);
   checkUI();
-  itemInput.value = '';
+  itemInput.value = "";
 };
 
 // Delete item
 const removeItem = (e) => {
-  if (e.target.parentElement.classList.contains('remove-item')) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
     e.target.parentElement.parentElement.remove();
     checkUI();
   }
@@ -56,20 +57,38 @@ const clearItems = (e) => {
   checkUI();
 };
 
+// Filter items
+const filterItems = (e) => {
+  const listItems = itemList.querySelectorAll("li");
+  const filterText = e.target.value.toLowerCase();
+
+  listItems.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(filterText) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+};
+
+// Update UI
 const checkUI = () => {
-  const listItems = itemList.querySelectorAll('li');
+  const listItems = itemList.querySelectorAll("li");
   if (listItems.length) {
-    clearButton.style.display = 'block';
-    itemFilter.style.display = 'block';
+    clearButton.style.display = "block";
+    itemFilter.style.display = "block";
   } else {
-    clearButton.style.display = 'none';
-    itemFilter.style.display = 'none';
+    clearButton.style.display = "none";
+    itemFilter.style.display = "none";
   }
 };
 
 // Event Listeners
-itemForm.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem);
-clearButton.addEventListener('click', clearItems);
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearButton.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
 
 checkUI();
